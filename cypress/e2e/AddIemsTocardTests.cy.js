b8ae2d7464350a15d2c49301cb6dc5c652d43fef
PirogDawid <pirog.dawid@outlook.com>
2023-04-10 18:22:10 +0200

    Add test

---
+++ cypress/e2e/AddIemsTocardTests.cy.js
@@ -0,0 +1,28 @@
+import Page from "../support/MainPageObjectModel.cy"
+const page = new Page
+const t1 = performance.now();
+const PageLoadTime = ((t1) / 1000);
+
+describe('Website shop tests ', function() {
+        
+    it('Add items to my cart tests', function() {
+         //Open main page
+        cy.openShirtPage()
+        cy.get('[href="/product-category/shirts/?add-to-cart=41"]').should("be.visible")
+        .click();
+        page.cartBar().click();
+             
+        })
+        it("Loading page",  function () {
+            //Open t-shirts page
+            cy.openShirtPage();
+            expect(PageLoadTime).to.be.lessThan(2) // Page load time
+            // Check status code 
+            cy.request({
+                url: 'https://skleptest.pl/product-category/shirts/',
+                followRedirect: false,
+              }).then(resp => {
+                expect(resp.status).to.eq(200) 
+        })
+    })
+})
