770129d3fbea5b16c86e5d2da3bf80ed3b7d5ae2
PirogDawid <pirog.dawid@outlook.com>
2023-04-27 20:55:40 +0200

    added data generation function

--- cypress/e2e/LoginTests.cy.js
+++ cypress/e2e/LoginTests.cy.js
@@ -42,11 +42,11 @@ describe("Account Page",  function () {
        login.usernameField().type(randomEmail)
             .should('have.value', (randomEmail));
        login.passwordField().type(`Test123456789!@`)
-            .should('have.value', 'Test123456789!@')       
+            .should('have.value', 'Test123456789!@');   
        login.loginButton().click();
        ///Error mesage when your username is incorrect
        cy.get("#post-8 > div.woocommerce > ul").should('be.visible')
-       .contains('A user could not be found with this email address.')
+       .contains('A user could not be found with this email address.');
                
 })
     it("Login with exist username and wrong password",  function () {
@@ -54,28 +54,28 @@ describe("Account Page",  function () {
        login.usernameField().type(`skleptestarmy17`)
             .should('have.value', 'skleptestarmy17');
        login.passwordField().type(randomPassword)
-            .should('have.value', (randomPassword))       
+            .should('have.value', (randomPassword));    
        login.loginButton().click();
        //Error mesage when your password is incorrect
        cy.get("#post-8 > div.woocommerce > ul").should('be.visible')
-       .contains(' The password you entered for the username ')
+       .contains(' The password you entered for the username ');
        //Lost your password mesage is visible
        cy.get("#post-8 > div.woocommerce > ul > li > a").should('be.visible')
-       .contains("Lost your password?")
+       .contains("Lost your password?");
        
 })
     it("Login with empty fields",  function () {
             
     login.usernameField()
-         .should('not.have.value')
+         .should('not.have.value');
     login.passwordField()
          .should('not.have.value')       
     login.loginButton().click();
     //Error mesage when your dont input username
     cy.get("#post-8 > div.woocommerce > ul").should('be.visible').and('exist')
-          .contains(' Username is required.')
+          .contains(' Username is required.');
     //Error should exist
-    cy.get('#post-8 > div.woocommerce > ul > li > strong').should('exist').and('be.visible')
+    cy.get('#post-8 > div.woocommerce > ul > li > strong').should('exist').and('be.visible');
                            
      })
 })
